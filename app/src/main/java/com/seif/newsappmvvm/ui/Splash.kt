d5dc516fc9148f793fef38d7d64c53b6652a04c7
seif <seifeldinmohamed101@gmail.com>
2021-05-03 21:52:33 +0200

    News App MVVM

--- app/src/main/java/com/seif/newsappmvvm/ui/Splash.kt
+++ app/src/main/java/com/seif/newsappmvvm/ui/Splash.kt
@@ -1,13 +1,25 @@
 package com.seif.newsappmvvm.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.seif.newsappmvvm.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class Splash : AppCompatActivity() {
+
+    private val splashTime: Long = 2000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        txt_companyName.alpha = 0f
+        txt_companyName.animate().setDuration(2000).alpha(1.5f)
+        Handler().postDelayed({
+            startActivity(Intent(this, NewsActivity::class.java))
+        }, splashTime)
+
 
     }
 }
